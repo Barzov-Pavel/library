2d95bcf25189932208b0bc01f8c3a20e8ce5708d
Pavel <50164365+Barzov-Pavel@users.noreply.github.com>
2019-05-01 21:36:38 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,51 @@
+﻿#ifndef HEADER_H
+#define HEADER_H
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <ostream>
+#include <Windows.h>
+#include <cstdio>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+class library												// Базовый класс
+{
+protected:
+	string author;
+	string title;
+	int year;
+	int amount;
+public:
+	library();
+	library(string title, int year);
+	void setTitle(string title);
+	void setYear(int year);
+	string getTitle();
+	int getYear();
+	friend istream & operator >> (istream & stream, library obj);
+	friend ostream& operator << (ostream & stream, library obj);
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+class book : public library								// Базовый класс 2 уровня
+{
+protected:
+
+	string levelDefend;
+public:
+	book();
+	book(string title, int year);
+	friend ostream& operator << (ostream & stream, book obj);
+};
+
+
+#endif
---
+++ Main.cpp
@@ -0,0 +1,87 @@
+﻿#include "Header.h"
+
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	book a1("О дивный новый мир", 2016);
+	book a2("О дивный новый мир", 2016);
+	book a3("Три товарища", 2016);
+	book a4("Мастер и Маргарита", 2012);
+	book a5("Финансист", 2015);
+
+	multimap<string, book> lib = { { "Олдос Хаксли" , a1 },{ "Олдос Хаксли" ,a2 },{ "Эрих Мария Ремарк" , a3 },
+	{ "Михаил Булгаков" , a4 },{ "Теодор Драйзер" , a5 } };
+
+	int variant;
+
+	cout << "Выберите вариант\n" << endl;
+	cout << "1. Добавить вновь поступившую книгу\n"
+		<< "2. Удалить списанную книгу\n"
+		<< "3. Выдать список всех книг упорядоченных по фамилиям\n"
+		<< "5. выйти\n" << endl;
+	cout << ">>> ";
+
+
+	do
+	{
+		string vvod_author;
+		string vvod_titel;
+		string erase_book;
+		book a6;
+		pair <string, book> A6;
+		int vvod_year;
+		cin >> variant;
+
+		switch (variant) {
+
+		case 1:
+			cout << "Введите автора: " << endl;
+			cin.ignore();
+			getline(cin, vvod_author);
+			cout << "Введите название: " << endl;
+			cin >> vvod_titel;
+			cin.ignore();
+			getline(cin, vvod_titel);
+			cout << "Введите год издания: " << endl;
+			cin >> vvod_year;
+			a6.setYear(vvod_year);
+
+			A6.first = vvod_author;
+			A6.second = a6;
+
+			lib.insert(A6);
+
+			break;
+		case 2:
+			cout << "Введите автора удаляемой книги " << endl;
+			cin.ignore();
+			getline(cin, erase_book);
+			lib.erase(erase_book);
+			break;
+
+		case 3:
+			for (auto a : lib)
+			{
+				cout << a.first << a.second << endl;
+			}
+			break;
+
+		case 5:
+			cout << "Выход из программы..." << endl;
+			exit(EXIT_SUCCESS);
+			break;
+
+		default:
+			cerr << "Вы выбрали неверный вариант" << endl;
+			exit(EXIT_FAILURE);
+		}
+
+	} while (variant != 5);
+
+
+	return 0;
+}
